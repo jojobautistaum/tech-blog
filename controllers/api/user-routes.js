8797b4f7396b1b84655a32f3faf1e4dbcf9e76eb
Jojo Bautista <gjojob@yahoo.com>
2022-01-24 11:44:36 -0600

    Added footer

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -110,7 +110,7 @@ router.post('/logout', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  // Example {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password123'}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
